2290748aa76b7cc12bb6a77a7065d9bfe7b72c3f
Bala021199 <balamurugan21199@gmail.com>
2022-01-06 12:03:20 -0800

    latest

--- Session/js/suddent/api.js
+++ Session/js/suddent/api.js
@@ -38,46 +38,55 @@ function getText() {
   fetch("https://jsonplaceholder.typicode.com/comments/")
     .then((res) => res.json())
     .then((x) => {
-      let tab = document.getElementById("table");
+      let tab = document.createElement("table");
+      document.body.appendChild(tab);
       let trow1 = document.createElement("tr");
       tab.appendChild(trow1);
-      var tdata1 = document.createElement("th");
-      trow1.appendChild(tdata1);
-      
 
       // ***************** FOR HEADER ************
       let z = Object.keys(Object.assign({}, ...x));
-      for (s = 0; s < z.length; s++) {
-        var r = z[s];
-        console.log(r);
+      for (i = 0; i < z.length; i++) {
+        var tdata1 = document.createElement("th");
+        trow1.appendChild(tdata1);
+        var r = z[i];
         var text = document.createTextNode(r);
         tdata1.appendChild(text);
-        console.log(text);
       }
 
-      
-      
-
-     
-      
-
-      for (let i = 1; i < x.length; i++) {
-        let id = document.createTextNode(x[i].id);
-        tdata2.appendChild(id);
-        let name = document.createTextNode(x[i].name);
-        tdata2.appendChild(name);
-        let email = document.createTextNode(x[i].email);
-        tdata2.appendChild(email);
-        let bdy = document.createTextNode(x[i].body);
-        tdata2.appendChild(bdy);
+      // // ********************* BODY *********************
+      for (let j = 0; j< x.length; j++) {
         let trow2 = document.createElement("tr");
-        tab.appendchid(trow2);
-        let tdata2 = document.createElement("td");
-        trow1.appendChild(tdata2)
-        
-        
-
+        tab.appendChild(trow2);
+        for (const [key, value] of Object.entries(x[j])) {
+          let tdata2 = document.createElement("td");
+          trow2.appendChild(tdata2);
+          let ans = document.createTextNode(value);
+          tdata2.appendChild(ans);
+        }
       }
+
+
+//       // *************** BODY ERROR ******************
+// for (let j = 0; j < 20; j++) {
+//   let trow2 = document.createElement("tr");
+//   tab.appendChild(trow2);
+//   for (let k = 0; k < r.length + 1; k++) {
+//       let tdata2 = document.createElement("td");
+//       trow2.appendChild(tdata2);
+//       let postid = document.createTextNode(x[k].postId);
+//       tdata2.appendChild(postid);
+//       let id = document.createTextNode(x[k].id);
+//       tdata2.appendChild(id);
+//       let name = document.createTextNode(x[k].name);
+//       tdata2.appendChild(name);
+//       let email = document.createTextNode(x[k].email);
+//       tdata2.appendChild(email);
+//       let bdy = document.createTextNode(x[k].body);
+//       tdata2.appendChild(bdy);
+//   }
+// }
+
     });
-  // document.body.appendChild(tab);
 }
+
+
